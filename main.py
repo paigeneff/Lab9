b3d5159917dfb52d01376b99298de99ac9dcf1eb
paigeneff <paigeneff@ufl.edu>
2024-04-10 08:27:12 -0400

    main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+menu = '''Menu
+-------------
+1. Encode
+2. Decode
+3. Quit'''
+
+def encode(password):
+    newpassword = ''
+    for num in password:
+        newpassword += str(int(num) + 3)
+    return newpassword
+
+if __name__ == '__main__':
+    while True:
+        print(menu)
+        option = int(input('Please enter an option: '))
+        if option == 1:
+            password = input('Please enter your password to encode: ')
+            encoded = encode(password)
+            print('Your password has been encoded and stored!')
+
